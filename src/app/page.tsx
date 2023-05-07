8f9ae285089188d55cd34420789186dcdc55cf02
Pedro Valério <pedroddjkK@gmail.com>
2023-05-07 14:51:43 -0300

    Segunda Página Feita

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,27 @@
 import { Inter, Kanit } from 'next/font/google'
 import { AiOutlineDown } from 'react-icons/ai'
 import ReactFullpage from '@fullpage/react-fullpage';
-import "./astronauta.png"
+import Image from 'next/image'
+import astronauta from "./astronauta.png"
 import "./style.css"
+import React from 'react';
 
 const inter = Inter({ subsets: ['latin'] })
 const kanit = Kanit({ weight: "600", subsets: ['latin'] })
 
 export default function Home() {
+  const [ currentText, setCurrentText ] = React.useState(0);
+
+  const textos = [
+    "OI O MEU NOME É ASTRONAUTA FUINHA!",
+    "VOU TE ENSINAR O QUE É UMA IA!",
+    "VAMOS LÁ?",
+    "IA É A ABREVIATURA DE INTELIGÊNCIA ARTIFICIAL",
+    "A INTELIGÊNCIA ARTIFICIAL CONSEGUE APRENDER COMO NÓS",
+    "A IA CONSEGUE AJUDAR A GENTE A RESOLVER PROBLEMAS",
+    "AJUDE A IA A APRENDER MAIS!"
+  ]
+
   return (
     <main className="">
       {/* @ts-expect-error */}
@@ -28,9 +42,18 @@ export default function Home() {
                   </button>
                 </div>
               </div>
-              <div className="section w-screen h-screen flex justify-center items-center second-page">
-                <div>
-                  <img src="./astronauta.png" alt="astronauta" />
+              <div className="section w-screen flex justify-center items-center second-page overflow-hidden" onClick={() => {
+                if (currentText < textos.length - 1) {
+                  setCurrentText(currentText + 1)
+                } else {
+                  fullpageApi.moveSectionDown()
+                }
+              }}>
+                <div className='flex flex-col'>
+                  <div className='bg-white outline outline-4 outline-black w-72 h-72 rounded-full self-end mt-6 flex justify-center items-center text-3xl font-bold drop-shadow-2xl text-center'>
+                    {currentText < textos.length ? textos[currentText] : null}
+                  </div>
+                  <Image src={astronauta} alt="astronauta"/>
                 </div>
               </div>
               <div className="section w-screen h-screen flex justify-center items-center third-page">
