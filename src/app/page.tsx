065138ebd3f5236a36cfea976b051c23acef8261
Pedro Aparecido Valério <pedroddjkk@gmail.com>
2023-05-08 08:34:33 -0300

    Terceira Pagina e Complemento de Imagens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,10 @@ import astronauta from "./astronauta.png"
 import balao from "./balao.png"
 import abacaxi from "./abacaxi.png"
 import maca from "./maca.png"
+import aviao from "./plane.png"
+import foguete from "./foguete.png"
+import cat from "./cat.webp"
+import dog from "./dog.webp"
 import "./style.css"
 import React from 'react';
 
@@ -14,7 +18,8 @@ const inter = Inter({ subsets: ['latin'] })
 const kanit = Kanit({ weight: "600", subsets: ['latin'] })
 
 export default function Home() {
-  const [ currentText, setCurrentText ] = React.useState(0);
+  const [currentText, setCurrentText] = React.useState(0);
+  const [currentImage, setCurrentImage] = React.useState(0);
 
   const textos = [
     "OI O MEU NOME É ASTRONAUTA FUINHA!",
@@ -26,6 +31,31 @@ export default function Home() {
     "AJUDE A IA A APRENDER MAIS!"
   ]
 
+  const images = [
+    {
+      correctImage: maca,
+      wrongImage: abacaxi,
+      text: "QUAL É A MAÇÃ?"
+    },
+    {
+      correctImage: aviao,
+      wrongImage: foguete,
+      text: "QUAL É O AVIÃO?"
+    },
+    {
+      correctImage: cat,
+      wrongImage: dog,
+      text: "QUAL É O GATO?"
+    }
+  ]
+
+  function onClickImage(fullpageApi: any) {
+    if (currentImage < images.length - 1) {
+      setCurrentImage(currentImage + 1)
+    } else {
+      fullpageApi.moveSectionDown()
+    }
+  }
   return (
     <main className="">
       {/* @ts-expect-error */}
@@ -39,9 +69,13 @@ export default function Home() {
               <div className={"section bg-pink-50 w-screen h-screen first-page flex justify-center items-center flex-col " + kanit.className}>
                 <div className='flex justify-center items-center flex-col'>
                   <p className='text-6xl text-center w-10/12 text-white drop-shadow-xl'>EXPLORE O MUNDO DAS  <span className='text-pink-500 drop-shadow-2xl'>IAs</span>!</p>
-                  <button onClick={() => fullpageApi.moveSectionDown()} className='bg-[#d83eff] flex items-center p-2 rounded-md my-12 outline-white outline-8 outline active:bg-[#ac31cc]'>
+                  <button onClick={() => {
+                    setCurrentImage(0)
+                    setCurrentText(0)
+                    fullpageApi.moveSectionDown()
+                  }} className='bg-[#d83eff] flex items-center p-2 rounded-md my-12 outline-white outline-8 outline active:bg-[#ac31cc]'>
                     <p className='text-3xl text-white px-6'>INICIAR</p>
-                    <AiOutlineDown className='text-white w-16 h-14 hover:cursor-pointer mx-6'/>
+                    <AiOutlineDown className='text-white w-16 h-14 hover:cursor-pointer mx-6' />
                   </button>
                 </div>
               </div>
@@ -56,25 +90,33 @@ export default function Home() {
                   <div className='bg-white outline outline-4 outline-black w-72 h-72 rounded-full self-end mt-6 flex justify-center items-center text-3xl font-bold drop-shadow-2xl text-center overflow-hidden'>
                     {currentText < textos.length ? textos[currentText] : null}
                   </div>
-                  <Image src={astronauta} alt="astronauta"/>
+                  <Image src={astronauta} alt="astronauta" />
                 </div>
               </div>
               <div className="section w-screen h-screen flex justify-center items-center third-page overflow-hidden">
                 <div className='flex flex-col justify-between items-center w-screen h-screen'>
                   <div className='flex flex-row justify-between items-center w-11/12 pt-20'>
-                    <div className='w-40 h-40 flex items-center justify-center bg-white outline outline-4 outline-black rounded-full'>
-                      <Image src={abacaxi} alt="abacaxi" width={150} height={150}/>
+                    <div className='w-40 h-40 flex items-center justify-center bg-white outline outline-4 outline-black rounded-full active:bg-slate-500' onClick={() => onClickImage(fullpageApi)}>
+                      <Image src={images[currentImage]?.wrongImage} alt="abacaxi" width={150} height={150} />
                     </div>
-                    <div className='w-40 h-40 flex items-center justify-center bg-white outline outline-4 outline-black rounded-full'>
-                      <Image src={maca} alt="balao" width={130} height={130}/>
+                    <div className='w-40 h-40 flex items-center justify-center bg-white outline outline-4 outline-black rounded-full active:bg-slate-500' onClick={() => onClickImage(fullpageApi)}>
+                      <Image src={images[currentImage]?.correctImage} alt="balao" width={130} height={130} />
                     </div>
-                  </div>  
+                  </div>
                   <div className='self-start flex flex-col w-full'>
                     <div className='bg-white outline outline-4 outline-black w-60 rounded-full self-end mt-6 flex justify-center items-center text-3xl font-bold drop-shadow-2xl text-center overflow-hidden mr-2 p-6'>
-                      <p>ME ENSINE! <br /> QUAL É A MAÇÃ?</p>
+                      <p>ME ENSINE! <br /> {images[currentImage]?.text}</p>
                     </div>
-                    <Image src={astronauta} alt="astronauta" width={300} height={100}/>
+                    <Image src={astronauta} alt="astronauta" width={300} height={100} />
                   </div>
+                </div>
+              </div>
+              <div className="section w-screen h-screen flex justify-center items-center second-page">
+                <div className='w-screen h-screen flex flex-col justify-center items-center'>
+                  <div className='bg-white outline outline-4 outline-black w-64 h-64 rounded-full self-end mt-6 flex justify-center items-center text-3xl font-bold drop-shadow-2xl text-center overflow-hidden'>
+                    APRENDI COM VOCÊ! <br /> OBRIGADO!
+                  </div>
+                  <Image src={astronauta} alt="astronauta" />
                 </div>
               </div>
             </ReactFullpage.Wrapper>
